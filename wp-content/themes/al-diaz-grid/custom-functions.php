c2ecbf053602c9b23843ab32230cc272e6c83152
Pea <pea@misfist.com>
2014-11-07 16:17:38 -0500

    Updated filter to enable category and tag filtering.

--- wp-content/themes/al-diaz-grid/custom-functions.php
+++ wp-content/themes/al-diaz-grid/custom-functions.php
@@ -3,6 +3,7 @@
 /*-----------------------------------------------------------------------------------*/
 # HomePage Taxonomy Filter
 /*-----------------------------------------------------------------------------------*/
+
 function tie_taxonomy_filter($taxonomy='category', $enable='disable', $exclude='') { 
 
 	// Is filter enabled and grid display selected OR is function called with enabled?
@@ -10,22 +11,22 @@ function tie_taxonomy_filter($taxonomy='category', $enable='disable', $exclude='
 
 	<ul id="filters">
 	  <li class="current all-items"><a href="#" data-filter="*"><?php _e( 'All' , 'tie' ) ?></a></li>
-		
-		<?php
-		// If $taxonomy isn't category (default), get tags
-		if($taxonomy != 'category') {
 
+		<?php
+		// Categories and Tags
+		if($taxonomy == 'all') { ?>
+
+			<?php
 			// Get tags
 			$tags = get_tags('exclude='.$exclude); 
 
 			// Display tags
 			foreach($tags as $tag) { ?>
 
-			<li><a href="#" data-filter=".tag-<?php echo $tag->slug ?>"><?php echo $tag->name ?></a></li>
+				<li><a href="#" data-filter=".tag-<?php echo $tag->slug ?>"><?php echo $tag->name ?></a></li>
 
 			<?php } ?>
 
-		<?php } else { ?>
 
 			<?php
 			// Check for excluded categories
@@ -40,7 +41,45 @@ function tie_taxonomy_filter($taxonomy='category', $enable='disable', $exclude='
 			// Display categories
 			foreach($categories as $category) { ?>
 
-			 <li><a href="#" data-filter=".cat_<?php echo $category->term_id ?>"><?php echo $category->name ?></a></li>
+				<li><a href="#" data-filter=".cat_<?php echo $category->term_id ?>"><?php echo $category->name ?></a></li>
+
+			<?php } ?>
+
+		<?php }
+
+		// Tags only
+		elseif($taxonomy == 'tag') { ?>
+
+			<?php
+			// Get tags
+			$tags = get_tags('exclude='.$exclude); 
+
+			// Display tags
+			foreach($tags as $tag) { ?>
+
+			<li><a href="#" data-filter=".tag-<?php echo $tag->slug ?>"><?php echo $tag->name ?></a></li>
+
+			<?php } ?>
+
+		<?php } 
+
+		// Categories only
+		else { ?>
+
+			<?php
+			// Check for excluded categories
+			$exc_home_cats = tie_get_option( 'exc_home_cats' );
+			if($exc_home_cats) {
+				$comma_cats_separated = @implode(",", $exc_home_cats );
+			}
+
+			// Get categories
+			$categories = get_categories('exclude='.$comma_cats_separated);
+
+			// Display categories
+			foreach($categories as $category) { ?>
+
+				<li><a href="#" data-filter=".cat_<?php echo $category->term_id ?>"><?php echo $category->name ?></a></li>
 
 			<?php } ?>
 
